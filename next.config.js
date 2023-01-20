6233cd075e3bd93673dd17a483d6518ddb63ef61
Marc Cilliers <selcouthphenakism@gmail.com>
2023-01-20 12:41:42 +0000

    Add youtube thumbnail remote pattern

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "static-cdn.jtvnw.net",
       },
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
+      },
     ],
   },
 };
